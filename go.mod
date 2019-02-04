29dfe42542c274cf0b1c43b3b15d6832eb5ab642
Danny Joyce <djoyce@pivotal.io>
2019-02-04 11:33:31 -0500

    Update dagger
    Co-authored-by: David Freilich <dfreilich@pivotal.io>

--- go.mod
+++ go.mod
@@ -2,14 +2,15 @@ module pip-cnb
 
 require (
 	github.com/buildpack/libbuildpack v1.10.0
-	github.com/cloudfoundry/dagger v0.0.0-20190131223759-275515017cba
+	github.com/cloudfoundry/dagger v0.0.0-20190204162931-79f83e7da5d1
 	github.com/cloudfoundry/libcfbuildpack v1.39.0
 	github.com/cloudfoundry/python-cnb v0.0.0-20190123155909-fe952f93647a
 	github.com/fatih/color v1.7.0
 	github.com/golang/mock v1.2.0
 	github.com/kr/pty v1.1.3 // indirect
+	github.com/mattn/go-colorable v0.1.0 // indirect
 	github.com/onsi/gomega v1.4.3
 	github.com/sclevine/spec v1.2.0
-	golang.org/x/sys v0.0.0-20190201152629-afcc84fd7533 // indirect
+	golang.org/x/sys v0.0.0-20190204103248-980327fe3c65 // indirect
 	gopkg.in/yaml.v2 v2.2.2
 )
--- integration/integration_test.go
+++ integration/integration_test.go
@@ -6,8 +6,6 @@ import (
 	"path/filepath"
 	"testing"
 
-	"github.com/buildpack/libbuildpack/buildpack"
-
 	"github.com/cloudfoundry/dagger"
 
 	"github.com/sclevine/spec"
@@ -30,37 +28,10 @@ func testIntegration(t *testing.T, when spec.G, it spec.S) {
 			uri, err := dagger.PackageBuildpack()
 			Expect(err).ToNot(HaveOccurred())
 
-			pythonCNBURI, err := dagger.GetRemoteBuildpack("https://github.com/cloudfoundry/python-cnb/releases/download/v0.0.1/python-cnb-0.0.1.tgz")
+			pythonCNBURI, err := dagger.GetLatestBuildpack("python-cnb")
 			Expect(err).ToNot(HaveOccurred())
 
-			builderMetadata := dagger.BuilderMetadata{
-				Buildpacks: []dagger.Buildpack{
-					{
-						ID:  "org.cloudfoundry.buildpacks.python",
-						URI: pythonCNBURI,
-					},
-					{
-						ID:  "org.cloudfoundry.buildpacks.pip",
-						URI: uri,
-					},
-				},
-				Groups: []dagger.Group{
-					{
-						[]buildpack.Info{
-							{
-								ID:      "org.cloudfoundry.buildpacks.python",
-								Version: "0.0.1",
-							},
-							{
-								ID:      "org.cloudfoundry.buildpacks.pip",
-								Version: "0.0.1",
-							},
-						},
-					},
-				},
-			}
-
-			app, err := dagger.Pack(filepath.Join("testdata", "simple_app"), builderMetadata, dagger.CFLINUXFS3)
+			app, err := dagger.PackBuild(filepath.Join("testdata", "simple_app"), pythonCNBURI, uri)
 			Expect(err).ToNot(HaveOccurred())
 
 			app.SetHealthCheck("", "3s", "1s")
@@ -69,17 +40,17 @@ func testIntegration(t *testing.T, when spec.G, it spec.S) {
 			err = app.Start()
 			if err != nil {
 				_, err = fmt.Fprintf(os.Stderr, "App failed to start: %v\n", err)
-				containerID, imageName, volumeIDs, err := app.ContainerInfo()
+				containerID, imageName, volumeIDs, err := app.Info()
 				Expect(err).NotTo(HaveOccurred())
 				fmt.Printf("ContainerID: %s\nImage Name: %s\nAll leftover cached volumes: %v\n", containerID, imageName, volumeIDs)
 
-				containerLogs, err := app.ContainerLogs()
+				containerLogs, err := app.Logs()
 				Expect(err).NotTo(HaveOccurred())
 				fmt.Printf("Container Logs:\n %s\n", containerLogs)
 				t.FailNow()
 			}
 
-			body, _, err := app.HTTPGetAll("/")
+			body, _, err := app.HTTPGet("/")
 			Expect(err).ToNot(HaveOccurred())
 			Expect(body).To(ContainSubstring("Hello, World!"))
 
@@ -92,37 +63,10 @@ func testIntegration(t *testing.T, when spec.G, it spec.S) {
 			uri, err := dagger.PackageBuildpack()
 			Expect(err).ToNot(HaveOccurred())
 
-			pythonCNBURI, err := dagger.GetRemoteBuildpack("https://github.com/cloudfoundry/python-cnb/releases/download/v0.0.1/python-cnb-0.0.1.tgz")
+			pythonCNBURI, err := dagger.GetLatestBuildpack("python-cnb")
 			Expect(err).ToNot(HaveOccurred())
 
-			builderMetadata := dagger.BuilderMetadata{
-				Buildpacks: []dagger.Buildpack{
-					{
-						ID:  "org.cloudfoundry.buildpacks.python",
-						URI: pythonCNBURI,
-					},
-					{
-						ID:  "org.cloudfoundry.buildpacks.pip",
-						URI: uri,
-					},
-				},
-				Groups: []dagger.Group{
-					{
-						[]buildpack.Info{
-							{
-								ID:      "org.cloudfoundry.buildpacks.python",
-								Version: "0.0.1",
-							},
-							{
-								ID:      "org.cloudfoundry.buildpacks.pip",
-								Version: "0.0.1",
-							},
-						},
-					},
-				},
-			}
-
-			app, err := dagger.Pack(filepath.Join("testdata", "simple_app_vendored"), builderMetadata, dagger.CFLINUXFS3)
+			app, err := dagger.PackBuild(filepath.Join("testdata", "simple_app"), pythonCNBURI, uri)
 			Expect(err).ToNot(HaveOccurred())
 
 			app.SetHealthCheck("", "3s", "1s")
@@ -131,22 +75,21 @@ func testIntegration(t *testing.T, when spec.G, it spec.S) {
 			err = app.Start()
 			if err != nil {
 				_, err = fmt.Fprintf(os.Stderr, "App failed to start: %v\n", err)
-				containerID, imageName, volumeIDs, err := app.ContainerInfo()
+				containerID, imageName, volumeIDs, err := app.Info()
 				Expect(err).NotTo(HaveOccurred())
 				fmt.Printf("ContainerID: %s\nImage Name: %s\nAll leftover cached volumes: %v\n", containerID, imageName, volumeIDs)
 
-				containerLogs, err := app.ContainerLogs()
+				containerLogs, err := app.Logs()
 				Expect(err).NotTo(HaveOccurred())
 				fmt.Printf("Container Logs:\n %s\n", containerLogs)
 				t.FailNow()
 			}
 
-			body, _, err := app.HTTPGetAll("/")
+			body, _, err := app.HTTPGet("/")
 			Expect(err).ToNot(HaveOccurred())
 			Expect(body).To(ContainSubstring("Hello, World!"))
 
 			Expect(app.Destroy()).To(Succeed())
 		})
 	})
-
 }
