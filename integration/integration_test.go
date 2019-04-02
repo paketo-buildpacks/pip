dbed0ad75633a42fb853c9757c476dbc6fb8088c
Tyler Phelan <tphelan@pivotal.io>
2019-04-02 17:24:02 -0400

    Add integration test for caching python dependencies
    [#164702324]
    
    Co-authored-by: Chhavi Kankaria <ckankaria@pivotal.io>

--- integration/integration_test.go
+++ integration/integration_test.go
@@ -19,15 +19,26 @@ func TestIntegration(t *testing.T) {
 }
 
 func testIntegration(t *testing.T, when spec.G, it spec.S) {
+	var (
+		uri string
+		err error
+	)
+
 	it.Before(func() {
 		RegisterTestingT(t)
+		uri, err = dagger.PackageBuildpack()
+		Expect(err).ToNot(HaveOccurred())
+	})
+
+	it.After(func() {
+		if uri != "" {
+			Expect(os.RemoveAll(uri)).To(Succeed())
+		}
 	})
 
 	when("building a simple app", func() {
-		it("runs a python app using pip", func() {
-			uri, err := dagger.PackageBuildpack()
-			Expect(err).ToNot(HaveOccurred())
 
+		it("runs a python app using pip", func() {
 			pythonCNBURI, err := dagger.GetLatestBuildpack("python-cnb")
 			Expect(err).ToNot(HaveOccurred())
 
@@ -56,13 +67,33 @@ func testIntegration(t *testing.T, when spec.G, it spec.S) {
 
 			Expect(app.Destroy()).To(Succeed())
 		})
+
+		it("caches reused modules for the same app, but downloads new modules ", func() {
+			pythonCNBURI, err := dagger.GetLatestBuildpack("python-cnb")
+			Expect(err).ToNot(HaveOccurred())
+
+			app, err := dagger.PackBuild(filepath.Join("testdata", "simple_app"), pythonCNBURI, uri)
+
+			Expect(err).ToNot(HaveOccurred())
+
+			app.SetHealthCheck("", "3s", "1s")
+			app.Env["PORT"] = "8080"
+			err = app.Start()
+			Expect(err).ToNot(HaveOccurred())
+
+			_, imgName, _, _ := app.Info()
+
+			rebuiltApp, err := dagger.PackBuildNamedImage(imgName, filepath.Join("testdata", "simple_app_more_packages"), pythonCNBURI, uri)
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(rebuiltApp.BuildLogs()).To(MatchRegexp("Using cached.*Flask"))
+			Expect(rebuiltApp.BuildLogs()).To(MatchRegexp("Downloading.*itsdangerous"))
+			Expect(rebuiltApp.Destroy()).To(Succeed())
+		})
 	})
 
 	when("building a simple app that is vendored", func() {
 		it("runs a python app using pip", func() {
-			uri, err := dagger.PackageBuildpack()
-			Expect(err).ToNot(HaveOccurred())
-
 			pythonCNBURI, err := dagger.GetLatestBuildpack("python-cnb")
 			Expect(err).ToNot(HaveOccurred())
 
