68b35544e6bafdcb3a6e3bcdf1319ca276b17c13
Rob Dimsdale-Zucker <robert.dimsdale@gmail.com>
2022-05-17 10:46:30 -0400

    Add support for specifying versions of Pip with format: "X.Y" (#261)
    - Pip releases versions X.Y instead of X.Y.0, so this change is necessary to support specifying the exact version X.Y
    - Previously X.Y would match the latest patch of X.Y.*, making it impossible to select X.Y if X.Y.Z also existed as a valid dependency.

--- detect_test.go
+++ detect_test.go
@@ -14,18 +14,19 @@ import (
 func testDetect(t *testing.T, context spec.G, it spec.S) {
 	var (
 		Expect = NewWithT(t).Expect
-		detect packit.DetectFunc
+
+		detect        packit.DetectFunc
+		detectContext packit.DetectContext
 	)
 
 	it.Before(func() {
 		detect = pip.Detect()
+		detectContext = packit.DetectContext{}
 	})
 
 	context("detection", func() {
 		it("returns a build plan that provides pip", func() {
-			result, err := detect(packit.DetectContext{
-				WorkingDir: "/working-dir",
-			})
+			result, err := detect(detectContext)
 			Expect(err).NotTo(HaveOccurred())
 
 			Expect(result.Plan).To(Equal(packit.BuildPlan{
@@ -53,9 +54,7 @@ func testDetect(t *testing.T, context spec.G, it spec.S) {
 			})
 
 			it("returns a build plan that provides the version of pip from BP_PIP_VERSION", func() {
-				result, err := detect(packit.DetectContext{
-					WorkingDir: "/working-dir",
-				})
+				result, err := detect(detectContext)
 				Expect(err).NotTo(HaveOccurred())
 
 				Expect(result.Plan).To(Equal(packit.BuildPlan{
@@ -78,6 +77,105 @@ func testDetect(t *testing.T, context spec.G, it spec.S) {
 						},
 					},
 				}))
+			})
+
+			context("when the provided version is of the form X.Y", func() {
+				it.Before(func() {
+					os.Setenv("BP_PIP_VERSION", "2.11")
+				})
+
+				it("selects the version X.Y.0", func() {
+					result, err := detect(detectContext)
+
+					Expect(err).NotTo(HaveOccurred())
+
+					Expect(result.Plan).To(Equal(packit.BuildPlan{
+						Provides: []packit.BuildPlanProvision{
+							{Name: pip.Pip},
+						},
+						Requires: []packit.BuildPlanRequirement{
+							{
+								Name: pip.CPython,
+								Metadata: pip.BuildPlanMetadata{
+									Build: true,
+								},
+							},
+							{
+								Name: pip.Pip,
+								Metadata: pip.BuildPlanMetadata{
+									Version:       "2.11.0",
+									VersionSource: "BP_PIP_VERSION",
+								},
+							},
+						},
+					}))
+				})
+			})
+
+			context("when the provided version is of the form X.Y.Z", func() {
+				it.Before(func() {
+					os.Setenv("BP_PIP_VERSION", "22.1.3")
+				})
+
+				it("selects the exact provided version X.Y.Z", func() {
+					result, err := detect(detectContext)
+
+					Expect(err).NotTo(HaveOccurred())
+
+					Expect(result.Plan).To(Equal(packit.BuildPlan{
+						Provides: []packit.BuildPlanProvision{
+							{Name: pip.Pip},
+						},
+						Requires: []packit.BuildPlanRequirement{
+							{
+								Name: pip.CPython,
+								Metadata: pip.BuildPlanMetadata{
+									Build: true,
+								},
+							},
+							{
+								Name: pip.Pip,
+								Metadata: pip.BuildPlanMetadata{
+									Version:       "22.1.3",
+									VersionSource: "BP_PIP_VERSION",
+								},
+							},
+						},
+					}))
+				})
+			})
+
+			context("when the provided version is of some other form", func() {
+				it.Before(func() {
+					os.Setenv("BP_PIP_VERSION", "some.other")
+				})
+
+				it("selects the exact provided version", func() {
+					result, err := detect(detectContext)
+
+					Expect(err).NotTo(HaveOccurred())
+
+					Expect(result.Plan).To(Equal(packit.BuildPlan{
+						Provides: []packit.BuildPlanProvision{
+							{Name: pip.Pip},
+						},
+						Requires: []packit.BuildPlanRequirement{
+							{
+								Name: pip.CPython,
+								Metadata: pip.BuildPlanMetadata{
+									Build: true,
+								},
+							},
+							{
+								Name: pip.Pip,
+								Metadata: pip.BuildPlanMetadata{
+									Version:       "some.other",
+									VersionSource: "BP_PIP_VERSION",
+								},
+							},
+						},
+					}))
+				})
 			})
 		})
 
